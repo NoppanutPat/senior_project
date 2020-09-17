a9a3568edd1b6ceb0568b5652cd709c1e0748266
Noppanut <noppanut.t@ku.th>
2020-09-17 12:28:42 +0700

    First commit

---
+++ ros/quadrotor/build/quad_sim/CMakeFiles/quad_liftforce_plugins.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quad_liftforce_plugins.dir/src/quad_liftforce_plugins.cpp.o"
+  "/home/pat/senior_project/ros/quadrotor/devel/lib/libquad_liftforce_plugins.pdb"
+  "/home/pat/senior_project/ros/quadrotor/devel/lib/libquad_liftforce_plugins.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quad_liftforce_plugins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
